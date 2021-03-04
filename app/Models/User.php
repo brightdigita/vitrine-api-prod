d9ecc535259314805e2cfaf84c0cadf46a22f718
Brightdigita <ibnahmad005@gmail.com>
2021-03-04 11:23:42 +0000

    Init

---
+++ app/Models/User.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Models;
+
+use App\Interfaces\MustVerifyAccount;
+use App\Traits\VerifyAccount;
+use Cog\Laravel\Ban\Traits\Bannable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable //implements MustVerifyAccount
+{
+    use HasFactory, Notifiable;
+    use SoftDeletes;
+    use HasApiTokens;
+    use Bannable;
+    use VerifyAccount;
+    use HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    protected $with = [
+        'company',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Determine if BannedAtScope should be applied by default.
+     *
+     * @return bool
+     */
+    public function shouldApplyBannedAtScope()
+    {
+        return true;
+    }
+
+    public function verifications()
+    {
+        return $this->hasMany(UserVerification::class);
+    }
+
+    /**
+     * Route notifications for the Nexmo channel.
+     *
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return string
+     */
+    public function routeNotificationForNexmo($notification)
+    {
+        return $this->phone;
+    }
+
+    public function company()
+    {
+        return $this->hasOne(Company::class);
+    }
+}
